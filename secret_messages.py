19640b3410f59a59153b9257bd673eac93a11708
Jimmy Smutek <jimmy@smutek.net>
2017-07-01 10:42:11 -0400

    Finish interface, implement caesar, setup affine

--- secret_messages.py
+++ secret_messages.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import caesar
+import atbash
 
 
 def clear_screen():
@@ -31,12 +33,11 @@ def goodbye():
 
 
 def program_loop():
-
     # available ciphers
-    available_ciphers = [
-        'caesar',
-        'atbash'
-    ]
+    available_ciphers = {
+        '1': 'caesar',
+        '2': 'atbash'
+    }
 
     # our program is currently running
     program_running = True
@@ -44,44 +45,75 @@ def program_loop():
 
     while program_running:
 
+        # print the available ciphers
         print("The available ciphers are:")
 
-        count = 1
-        for cipher in available_ciphers:
-            print(str(count) + ". {}".format(str.capitalize(cipher)))
-            count += 1
+        for option, cipher in available_ciphers.items():
+            print("{}: {}".format(option, cipher))
 
         print("Choose a cipher by entering its corresponding number below. Enter QUIT to quit.\n")
 
-        user_input = input(input_char)
+        # Collect user input
+        user_input = str.lower(input(input_char))
         # -todo test user input here
-        selected_cipher = available_ciphers[int(user_input) - 1]
 
         # user has entered quit
-        if selected_cipher == "quit":
+        if user_input == "quit":
             break
 
-        if selected_cipher in available_ciphers:
-            # we have valid input, move on
+        # if the users choice is legit...
+        if user_input in available_ciphers:
+            # we have valid input,
+            # we are executing a cipher
             executing_cipher = True
+
             clear_screen()
+            selected_cipher = available_ciphers[user_input]
+
             while executing_cipher:
 
+                options = {
+                    "1": "encrypting",
+                    "2": "decrypting"
+                }
+                # ask if the user is encrypting or decrypting
                 print("You've selected {}, are you encrypting or decrypting?".format(str.capitalize(selected_cipher)))
                 print("Enter 1 if encrypting, or 2 if decrypting. Enter BACK to return to the main menu.\n")
 
-                user_input = input(input_char)
-                encrypt_or_decrypt = str.lower(user_input)
+                encrypt_or_decrypt = str.lower(input(input_char))
 
                 if encrypt_or_decrypt == "back":
                     break
+                # if the user selection is a valid option
+                elif encrypt_or_decrypt in options:
 
-                if encrypt_or_decrypt == "1":
-                    print("Encrypting....")
+                    # okay we're good, grab the desired action as a var so we can reference it below
+                    action = options[encrypt_or_decrypt]
+
+                    # get the message form the user
+                    print("Enter the text you will be {}:".format(action))
+                    users_message = str.lower(input(input_char))
+
+                    # okay, now we know which cipher, the action to take, and the message,
+                    # create an instance of the cipher
+
+                    # Caesar Cipher
+                    if selected_cipher == "caesar":
+                        # create an instance of caesar
+                        instance = caesar.Caesar()
+                    # atbash
+                    elif selected_cipher == "atbash":
+                        # create an instance of atbash
+                        instance = atbash.Atbash()
+
+                    # run the selected action on the created instance
+                    if action == "encrypting":
+                        print(instance.encrypt(users_message))
+                    else:
+                        print(instance.decrypt(users_message))
+
                     executing_cipher = False
-                elif encrypt_or_decrypt == "2":
-                    print("Decrypting")
-                    executing_cipher = False
+
                 else:
                     print("\nError. {} is not a valid option, please try again...\n".format(encrypt_or_decrypt))
 
@@ -95,10 +127,10 @@ def program_loop():
         if input(input_char).lower() != "n":
             program_loop()
 
+
 # greet the user
 hello()
 # run the program
 program_loop()
 # we were never here
 goodbye()
-
